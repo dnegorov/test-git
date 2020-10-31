301833bdb7c5ea041efe0cf531e91d9f3df4c672
Denis Egorov <55288899+dnegorov@users.noreply.github.com>
2020-10-31 22:45:21 +0300

    v20

---
+++ release.ps1
@@ -0,0 +1,16 @@
+$num = $args[0]
+$tag_ver = "v0.0." + $num.ToString()
+echo $tag_ver
+
+$f = (Get-Content .\README.md) 
+$f[6] = 'New version! ' + $tag_ver
+$f | set-content .\README.md
+
+$commit = "v" + $num.ToString()
+$tag_descr = 'test tag ' + $num.ToString()
+
+git add .
+git commit -m $commit
+git push
+git tag -a $tag_ver -m $tag_descr
+git push --tags
